060e5a99bce805d0f0a0777e759e978f91a34a1e
Lokesh Sharma <104786100+lokeshkavisth@users.noreply.github.com>
2023-09-11 12:54:15 +0530

    refactor: Refactor code for improved performance and readability

--- components/ui/buttons/Button.jsx
+++ components/ui/buttons/Button.jsx
@@ -5,7 +5,7 @@ const Button = (props) => {
   return (
     <Link
      {...props}
-      className="px-4 py-2 rounded-full text-base min-w-max max-w-max border border-black_05 bg-black_04 text-white_01 hover:bg-black_02 transition-all font-medium flex items-center gap-2"
+      className="px-4 py-2 rounded-full text-sm md:text-base min-w-max max-w-max border border-black_05 bg-black_04 text-white_01 hover:bg-black_02 transition-all font-medium flex items-center gap-2"
     >
       {props.text}
       {props.icon}
--- components/ui/cards/CardOne.jsx
+++ components/ui/cards/CardOne.jsx
@@ -5,6 +5,7 @@ const CardOne = ({
   src,
   title,
   description,
+  stack,
   cardClass,
   cardImgClass,
   width,
@@ -25,8 +26,13 @@ const CardOne = ({
       </div>
 
       <div className="space-y-2">
-        <h3 className="text-lg">{title}</h3>
+        <h3 className="text-lg capitalize">{title}</h3>
         <p>{description}</p>
+{stack && <ul className="flex items-center flex-wrap gap-2">
+  {stack.map((tech, index)=>(
+    <li key={index} className="border border-black_02 py-1 px-2 rounded-md bg-black_03 hover:bg-black_02 hover:bg-opacity-50 transition-all">{tech}</li>
+  ))}
+</ul>}
       </div>
     </div>
   );
