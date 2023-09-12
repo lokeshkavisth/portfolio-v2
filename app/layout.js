bf92e7ada3eb6a017aa9885f7c7e0013a6d13051
Lokesh Sharma <104786100+lokeshkavisth@users.noreply.github.com>
2023-09-12 17:54:51 +0530

    cleanup: remove commented code

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-// "use client";
-// import { useEffect } from "react";
 import Navbar from "@/components/ui/navigation/Navbar";
 import "@/app/globals.css";
 
@@ -47,31 +45,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  // useEffect(() => {
-  //   // Disable right-click
-  //   window.addEventListener("contextmenu", function (e) {
-  //     e.preventDefault();
-  //   });
-
-  //   // Disable Ctrl+C (or Cmd+C on Mac)
-  //   window.addEventListener("keydown", function (e) {
-  //     if ((e.ctrlKey || e.metaKey) && e.key === "c") {
-  //       e.preventDefault();
-  //     }
-  //   });
-
-  //   // Cleanup event listeners on unmount (optional)
-  //   return () => {
-  //     window.removeEventListener("contextmenu", function (e) {
-  //       e.preventDefault();
-  //     });
-  //     window.removeEventListener("keydown", function (e) {
-  //       if ((e.ctrlKey || e.metaKey) && e.key === "c") {
-  //         e.preventDefault();
-  //       }
-  //     });
-  //   };
-  // }, []);
   return (
     <html lang="en" className="overflow-x-hidden">
       <body className="bg-black_01 text-white_02 font-JetBrains overflow-x-hidden">
